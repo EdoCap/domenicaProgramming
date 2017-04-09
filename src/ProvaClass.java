cdced752aa72e54429641e1ec96827e348f2748d
Edoardo <edoardo.cappello@gmail.com>
2017-04-09 10:22:13 +0200

    reinizializzazione

--- src/ProvaClass.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Edoardo on 09/04/17.
- */
-public class ProvaClass {
-}
--- src/client/Main.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-/**
- * Created by Pietro on 09/04/2017.
- */
-public class Main {
-
-}
